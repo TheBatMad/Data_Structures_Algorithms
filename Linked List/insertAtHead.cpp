b84819491f4fd4feabc99272aa062d3e487bbf13
Madhurjya Sarma <48011226+TheBatMad@users.noreply.github.com>
2021-08-20 14:33:22 +0530

    Create insertAtHead.cpp

---
+++ Linked List/insertAtHead.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node* next;
+
+    node(int val)           //Constructor
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node* &head, int val)
+{   
+    node* n = new node(val);
+    n->next = head;           //next of n now points to the first node i.e. head
+    head = n;                 //head moved to n since n became the first
+}
+
+//To print our Linked List we make a function
+
+void display(node* head)      //Call by value since we aren't modifying the linked list here
+{
+    node* temp = head;        //to store the value of head
+    while(temp!=NULL)
+    {
+        cout << temp->data << "-";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    node* head = NULL;
+    insertAtHead(head,1);
+    insertAtHead(head,2);
+    insertAtHead(head,3);
+    display(head);
+}

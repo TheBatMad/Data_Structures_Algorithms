a279fd7adadf69a1bd93b98114856dfa25baae54
Madhurjya Sarma <48011226+TheBatMad@users.noreply.github.com>
2021-10-19 13:18:39 +0530

    Update circularLL.cpp

--- Linked List/circularLL.cpp
+++ Linked List/circularLL.cpp
@@ -57,6 +57,43 @@ void insertAtTail(node *&head, int val)
     n->next = head;
 }
 
+void deleteAtHead(node *&head)
+{
+    node *temp = head;
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+
+    node *todelete = head;
+    temp->next = head->next;
+    head = head->next;
+
+    delete todelete;
+}
+
+void deletion(node *&head, int pos)
+{
+    if (pos == 1)
+    {
+        deleteAtHead(head);
+        return;
+    }
+    node *temp = head;
+    int count = 1;
+
+    while (count != pos - 1)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+
+    delete todelete;
+}
+
 void display(node *head)
 {
     node *temp = head;
@@ -68,14 +105,18 @@ void display(node *head)
     cout << endl;
 }
 
-int main(){
-    node* head = NULL;
-    insertAtTail(head,1);
-    insertAtTail(head,2);
-    insertAtTail(head,3);
-    insertAtTail(head,4);
+int main()
+{
+    node *head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
     display(head);
-    insertAtHead(head,5);
+    insertAtHead(head, 5);
+    display(head);
+    deletion(head, 5);
     display(head);
 
+    return 0;
 }
